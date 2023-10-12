f22a95b9e229722f08a269d12e7377762fe204d0
azizchannoufi <channoufi2003@gmail.com>
2023-10-12 10:08:15 +0100

    fpush

---
+++ script.js
@@ -0,0 +1,16 @@
+// $('.signup').hide()
+$('.student').hide()
+$('.search').hide()
+var user="user"
+var password="12345678"
+$('.btn1').on('click',function(){
+    // if(($('#user').value==user)){
+    //     if($('#password').value==password){
+            $('.signup').hide()
+            $('.student').show()
+            $('.search').show()
+        }
+    // }
+
+// }
+)
